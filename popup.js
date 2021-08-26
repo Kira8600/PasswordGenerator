8e74da710be30b5b8cc09ada626ecf83bc2b3cf7
Jonas <Jonasattlan@gmail.com>
2021-08-27 00:15:09 +0200

    1.00

--- popup.js
+++ popup.js
@@ -10,13 +10,17 @@ document.getElementById("submit-button").addEventListener("click", generate);
 
 function generate() {
     var plen = document.getElementById("pw-lenght").value
-    var rchar = "" // Math.floor(Math.random() * 127) + 41
+    var rchar = ""
     var generated = ""
     var k = 0
     if (plen < 2049) {
         for (let i = 0; i < plen; i++) {
-            k = Math.random() * (127 - 41) + 41
+            k = Math.floor(Math.random() * (126 - 33) + 33)
+            console.log(k)
             rchar = String.fromCharCode(k)
+            if (k == 60 || k  == 62) {
+                rchar = String.fromCharCode(Math.random() * (59 - 33) + 33)
+            }
             generated += String(rchar)
           }
           document.getElementById("password").innerHTML = generated
